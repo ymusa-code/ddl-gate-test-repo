6783c86b0e3dfaa954fe8d5c8c0103fe95fc5d88
Yusuf Musa <ymusa@webmd.net>
2026-09-02 12:39:38 +0100

    DEMO: DDL append

---
+++ migrations/0010_demo_append_ddl.sql
@@ -0,0 +1,3 @@
+-- DEMO: SECOND structural push while the first ticket is still open -> dedup appends this script
+-- as a comment on the existing ticket instead of creating a new one.
+DROP TABLE IF EXISTS omnisync.dsarc_demo_noop_2;
